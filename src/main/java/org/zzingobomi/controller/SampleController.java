225afc1e0d42be5c190a4379ce09d6b96a4f8a68
JONGCHAN <zzingo5@naver.com>
2017-12-18 10:40:56 +0900

    sample init db

--- src/main/java/org/zzingobomi/controller/SampleController.java
+++ src/main/java/org/zzingobomi/controller/SampleController.java
@@ -37,7 +37,7 @@ public class SampleController {
 	public void addlist() {
 		
 		///
-		/// MovieGameItem
+		/// MovieGameItem -----------------------------------------------------------------
 		///
 		
 		// Sample
@@ -57,7 +57,7 @@ public class SampleController {
 		intheroom.setDirector("zzingo");
 		intheroom.setDescription("This is InTheRoom");
 		intheroom.setPoster("intheroomposter");
-		intheroom.setStartmfno(1L);
+		intheroom.setStartmfno(8L);
 		
 		movieGameItemRepository.save(intheroom);
 		
@@ -76,10 +76,10 @@ public class SampleController {
 		
 		
 		///
-		/// MovieFlowInfo
+		/// MovieFlowInfo -----------------------------------------------------------------
 		///
 		
-		// Sample1
+		// Sample1		// 1
 		MovieFlowInfo movieFlowInfo1 = new MovieFlowInfo();
 		movieFlowInfo1.setFilename("Chapter_01");
 		movieFlowInfo1.setMovietotaltime(20F);
@@ -94,7 +94,7 @@ public class SampleController {
 		movieFlowInfoRepository.save(movieFlowInfo1);
 		
 		
-		// Sample2
+		// Sample2		// 2
 		MovieFlowInfo movieFlowInfo2 = new MovieFlowInfo();
 		movieFlowInfo2.setFilename("Chapter_02");
 		movieFlowInfo2.setMovietotaltime(20F);
@@ -108,7 +108,7 @@ public class SampleController {
 		  
 		movieFlowInfoRepository.save(movieFlowInfo2);
 		
-		// Sample3
+		// Sample3		// 3
 		MovieFlowInfo movieFlowInfo3 = new MovieFlowInfo();
 		movieFlowInfo3.setFilename("Chapter_03");
 		movieFlowInfo3.setMovietotaltime(20F);
@@ -121,7 +121,7 @@ public class SampleController {
 		  
 		movieFlowInfoRepository.save(movieFlowInfo3);
 		
-		// Sample4
+		// Sample4		// 4
 		MovieFlowInfo movieFlowInfo4 = new MovieFlowInfo();
 		movieFlowInfo4.setFilename("Chapter_04");		
 		movieFlowInfo4.setMovietotaltime(20F);
@@ -129,7 +129,7 @@ public class SampleController {
 		  
 		movieFlowInfoRepository.save(movieFlowInfo4);
 		
-		// Sample5
+		// Sample5		// 5
 		MovieFlowInfo movieFlowInfo5 = new MovieFlowInfo();
 		movieFlowInfo5.setFilename("Chapter_05");	
 		movieFlowInfo5.setMovietotaltime(20F);
@@ -137,7 +137,7 @@ public class SampleController {
 		  
 		movieFlowInfoRepository.save(movieFlowInfo5);
 		
-		// Sample6
+		// Sample6		// 6
 		MovieFlowInfo movieFlowInfo6 = new MovieFlowInfo();
 		movieFlowInfo6.setFilename("Chapter_06");		
 		movieFlowInfo6.setMovietotaltime(20F);
@@ -145,13 +145,101 @@ public class SampleController {
 		  
 		movieFlowInfoRepository.save(movieFlowInfo6);
 		
-		// Sample7
+		// Sample7		// 7
 		MovieFlowInfo movieFlowInfo7 = new MovieFlowInfo();
 		movieFlowInfo7.setFilename("Chapter_07");	
 		movieFlowInfo7.setMovietotaltime(20F);
 		movieFlowInfo7.setMovieselecttime(10F);
 		  
 		movieFlowInfoRepository.save(movieFlowInfo7);
+		
+		
+		///////////////////////////////////////////////////////////////////////////////
+		
+		
+		// InTheRoom_01		// 8
+		MovieFlowInfo intheroom01 = new MovieFlowInfo();
+		intheroom01.setFilename("InTheRoom_01");	
+		intheroom01.setMovietotaltime(70F);
+		intheroom01.setMovieselecttime(62F);
+		
+		List<Long> intheroomNextfilefnos = new ArrayList<>(); 
+		intheroomNextfilefnos.add(9L);
+		intheroomNextfilefnos.add(10L); 
+	
+		intheroom01.setNextfilefnos(intheroomNextfilefnos);
+		  
+		movieFlowInfoRepository.save(intheroom01);
+		
+		// InTheRoom_02_01		// 9
+		MovieFlowInfo intheroom02_01 = new MovieFlowInfo();
+		intheroom02_01.setFilename("InTheRoom_02_01");	
+		intheroom02_01.setMovietotaltime(70F);
+		intheroom02_01.setMovieselecttime(62F);
+		  
+		movieFlowInfoRepository.save(intheroom02_01);
+		
+		// InTheRoom_02_02		// 10
+		MovieFlowInfo intheroom02_02 = new MovieFlowInfo();
+		intheroom02_02.setFilename("InTheRoom_02_02");	
+		intheroom02_02.setMovietotaltime(70F);
+		intheroom02_02.setMovieselecttime(62F);
+		  
+		movieFlowInfoRepository.save(intheroom02_02);
+		
+		
+		///////////////////////////////////////////////////////////////////////////////
+		
+		
+		// Tutorial_01		// 11
+		MovieFlowInfo tutorial01 = new MovieFlowInfo();
+		tutorial01.setFilename("Tutorial_01");	
+		tutorial01.setMovietotaltime(70F);
+		tutorial01.setMovieselecttime(45F);
+		
+		List<Long> tutorialNextfilefnos = new ArrayList<>(); 
+		tutorialNextfilefnos.add(12L);
+		tutorialNextfilefnos.add(13L); 
+	
+		tutorial01.setNextfilefnos(tutorialNextfilefnos);
+		  
+		movieFlowInfoRepository.save(tutorial01);
+		
+		// Tutorial_02_01		// 12
+		MovieFlowInfo tutorial02_01 = new MovieFlowInfo();
+		tutorial02_01.setFilename("Tutorial_02_01");	
+		tutorial02_01.setMovietotaltime(70F);
+		tutorial02_01.setMovieselecttime(0F);
+		
+		List<Long> tutorial02_01Nextfilefnos = new ArrayList<>(); 
+		tutorial02_01Nextfilefnos.add(14L);
+		tutorial02_01Nextfilefnos.add(14L); 
+	
+		tutorial02_01.setNextfilefnos(tutorial02_01Nextfilefnos);
+		  
+		movieFlowInfoRepository.save(tutorial02_01);
+		
+		// Tutorial_02_02		// 13
+		MovieFlowInfo tutorial02_02 = new MovieFlowInfo();
+		tutorial02_02.setFilename("Tutorial_02_02");	
+		tutorial02_02.setMovietotaltime(70F);
+		tutorial02_02.setMovieselecttime(0F);
+		
+		List<Long> tutorial02_02Nextfilefnos = new ArrayList<>(); 
+		tutorial02_02Nextfilefnos.add(14L);
+		tutorial02_02Nextfilefnos.add(14L); 
+	
+		tutorial02_02.setNextfilefnos(tutorial02_02Nextfilefnos);
+		  
+		movieFlowInfoRepository.save(tutorial02_02);
+		
+		// Tutorial_03		// 14
+		MovieFlowInfo tutorial03 = new MovieFlowInfo();
+		tutorial03.setFilename("Tutorial_03");	
+		tutorial03.setMovietotaltime(70F);
+		tutorial03.setMovieselecttime(0F);		
+		  
+		movieFlowInfoRepository.save(tutorial03);
 	}
 
 	/*
